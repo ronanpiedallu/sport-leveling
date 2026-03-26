6471b827ea67b798d63928b1e832633db958e1c7
ronanpiedallu <ronanpiedallu@gmail.com>
2026-03-26 07:30:33 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,9 @@
 self.addEventListener("install", () => {
-  console.log("SW installé");
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener("fetch", () => {});
